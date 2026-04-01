cb2cd718a00363f71923f95123b557a714c88f3d
Igor Mariak <i.mariak@outlook.com>
2026-04-01 10:51:04 +0200

    Add some data from json file

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import TypingArea from "./typing/page";
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-col justify-center items-center p-10 bg-amber-50">
-          <h1 className="font-retro text-3xl text-yellow-400 mb-2">BASH HERO </h1>
-          <p className="font-retro text-sm text-gray-400 tracking-widest"> your way to become a programist</p>
+      <div className="flex flex-col justify-center items-center p-10 bg-blue-950">
+          <h1 className="font-retro text-3xl text-white mb-2">BASH HERO </h1>
+          <p className="font-retro text-sm text-neutral-50 tracking-widest"> your way to become a programist</p>
       </div>
 
       <div>
--- app/typing/page.tsx
+++ app/typing/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 import { useKeyboardSchortcuts } from "../hooks/useKeyboardShortcuts";
 import { useTimer } from "../hooks/useTimer";
+import { useLevel } from "../hooks/useLevel";
 
 export default function TypingArea() {
   const { text, handleChange, duration, targetText } = useTimer();
   const { handleKeyDown } = useKeyboardSchortcuts();
+  const { level } = useLevel("level-1");
 
   return (
     <div className="flex flex-col items-center-safe justify-center p-10">
+        <div className="flex flex-col items-center mb-10 gap-2">
+            <h1 className="text-3xl text-blue-800"> { level?.title } </h1>
+            <p> { level?.description }</p>
+        </div>
         <h2> Tekst do wpisywania</h2>
         <p className="font-bold text-xl"> {targetText} </p>
         <div className="flex flex-col p-4 w-full max-w-md mx-auto">
@@ -16,7 +22,8 @@ export default function TypingArea() {
             onChange={handleChange}
             onKeyDown={handleKeyDown}
             placeholder="Zacznij pisać..."
-            className="border-2 border-gray-300 rounded-md p-3 mb-4 resize-none shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-amber-100 transition duration-200"
+            className="border-2 border-gray-300 rounded-md p-3 mb-4 resize-none shadow-sm 
+            focus:outline-none focus:ring-3 focus:ring-gray-100 focus:border-blue-900 transition duration-200"
             rows={2}
         />
 
