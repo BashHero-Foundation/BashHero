dd46a4938ed2470eef33244ef475966ec4b7a185
Igor Mariak <i.mariak@outlook.com>
2026-04-14 12:13:20 +0200

    Add other changes from review

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -1,6 +1,8 @@
 import levelsData from "../../levels/chapter1.json";
 import { TypingView } from "@/components/TypingView";
 
+export const dynamicParams = false;
+
 export default async function Page({ params }: { params: { id: string } }) {
   const {id} = await params
   const level = levelsData.levels.find(l => l.id === id);
--- app/hooks/useTypingLevel.ts
+++ app/hooks/useTypingLevel.ts
@@ -8,6 +8,8 @@ export function useTypingLevel(commands: Command[]) {
     const [startTime, setStartTime] = useState<number | null>(null);
     const [duration, setDuration] = useState(0);
 
+    const isFinished = currentIndex >= commands.length || (currentIndex === commands.length - 1 && duration > 0);
+
     const currentCommand = commands[currentIndex];
     if (!currentCommand) {
         return {
@@ -25,45 +27,46 @@ export function useTypingLevel(commands: Command[]) {
     const targetText = currentCommand?.text || "";
 
     const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const newText = e.target.value;
+        if (isFinished) return; // do not allow to change text if level is finished
+        const newText = e.target.value;
 
-    if (newText.length > targetText.length) return; // not allow to write more
+        if (newText.length > 2 * targetText.length) return; // not allow to write more
 
-    // start with first character
-    if (startTime === null && newText.length === 1) {
-        setStartTime(Date.now());
-    }
-
-    setText(newText);
-
-    if (startTime !== null && newText === targetText) {
-        if (currentIndex < commands.length - 1) {
-        // next command
-        setCurrentIndex(prev => prev + 1);
-        setText('');
-        } else {
-        // end of level
-        const end = Date.now();
-        setDuration(Number(((end - startTime) / 1000).toFixed(2)));
+        // start with first character
+        if (startTime === null && newText.length === 1) {
+            setStartTime(Date.now());
         }
-    }
-    };
 
-    const reset = () => {
-        setText('');
-        setCurrentIndex(0);
-        setStartTime(null);
-        setDuration(0);
-    };
+        setText(newText);
 
-    return {
-        text,
-        handleChange,
-        duration,
-        currentCommand,
-        currentIndex,
-        totalCommands: commands.length,
-        isFinished: duration > 0,
-        reset
-    };
+        if (startTime !== null && newText === targetText) {
+            if (currentIndex < commands.length - 1) {
+            // next command
+            setCurrentIndex(prev => prev + 1);
+            setText('');
+            } else {
+            // end of level
+            const end = Date.now();
+            setDuration(Number(((end - startTime) / 1000).toFixed(2)));
+            }
+        }
+        };
+
+        const reset = () => {
+            setText('');
+            setCurrentIndex(0);
+            setStartTime(null);
+            setDuration(0);
+        };
+
+        return {
+            text,
+            handleChange,
+            duration,
+            currentCommand,
+            currentIndex,
+            totalCommands: commands.length,
+            isFinished,
+            reset
+        };
 }
