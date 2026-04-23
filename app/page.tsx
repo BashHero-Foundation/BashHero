789696bcb01ec3d971728fec06b7773a6576394a
Igor Mariak <i.mariak@outlook.com>
2026-04-23 09:22:36 +0200

    Improve play button

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,13 @@ export default function Home() {
   return (
     <div>
       <div className="flex justify-center mt-20">
-        <h1 className="text-5xl text-text-secondary font-retro"> <a href="/game/1"> !! P L A Y !!</a> </h1>
+        <a
+          href="/game/1"
+          className="relative text-5xl font-retro px-8 py-6 border-4 border-text-secondary text-text-secondary rounded-4xl 
+          animate-bounce shadow-2xl hover:animate-pulse hover:scale-110 transition-all duration-75"
+        >
+          !! P L A Y !!
+        </a>
       </div>
 
       <div className="mt-40 px-10 text-xs"> 
