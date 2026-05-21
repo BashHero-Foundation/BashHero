c8fb688b978d2b4286b526ae08ee79434d3d3e16
Dominik <dommal13@st.amu.edu.pl>
2026-05-21 16:33:23 +0200

    chapter_separation

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,6 +1,11 @@
 "use client";
 
 import levelsData from "@/app/levels/chapter1.json";
+import chapter1 from "@/app/levels/chapter1.json";
+import chapter2 from "@/app/levels/chapter2.json";
+import chapter3 from "@/app/levels/chapter3.json";
+
+const chapters = [chapter1, chapter2, chapter3];
 
 export const Menu = () => {
   return (
@@ -11,7 +16,19 @@ export const Menu = () => {
       </summary>
 
       <div className="mt-3 flex flex-col gap-3 rounded-xl bg-blue-950/30 p-3">
-        {levelsData.levels.map((level) => (
+        {chapters.map((chapter) => (
+          <details
+          key={chapter.chapterId}
+          className="group/chapter rounded-xl border border-blue-900 bg-blue-900/30" 
+          open>
+          <summary className="cursor-pointer list-none rounded-xl px-4 py-3 text-white font-semibold select-none flex items-center justify-between">
+          <span>{chapter.chapterTitle}</span>
+          <span className="transition-transform duration-300 group-open/chapter:rotate-180">▼</span>
+          </summary>
+          
+        
+      <div className="px-2 pb-2 flex flex-col gap-2">
+        {chapter.levels.map((level) => (
           <a
             key={level.id}
             href={`/game/${level.id}`}
@@ -19,6 +36,9 @@ export const Menu = () => {
           >
             {level.title}
           </a>
+        ))}
+          </div>
+        </details>
         ))}
       </div>
     </details>
